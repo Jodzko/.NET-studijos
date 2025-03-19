aad6c24a3b15540fcef388a0a4ccd1110e34b025
Jodzko <A.Jodzko@gmail.com>
2025-03-19 22:10:20 +0200

    3_paskaita_web_API

---
+++ _3_paskaita_web_API/Car.cs
@@ -0,0 +1,17 @@
+﻿namespace _3_paskaita_web_API
+{
+    public class Car
+    {
+        public Guid Id;
+        public string Model { get; set;}
+        public string Color { get; set; }
+        
+
+        public Car(string model, string color)
+        {
+            Id = new Guid();
+            Model = model;
+            Color = color;
+        }
+    }
+}
---
+++ _3_paskaita_web_API/Controllers/CarController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _3_paskaita_web_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        private readonly ICarDatabase _carDatabase;
+        
+        public CarController(ICarDatabase cardatabase)
+        {
+            _carDatabase = cardatabase;
+        }
+
+        [HttpPost]
+        public ActionResult AddCar([FromBody] Car car)
+        {
+            _carDatabase.AddCar(car);
+            return Ok();
+        }
+
+        [HttpGet]
+        public ActionResult GetCars()
+        {
+            return Ok(_carDatabase.Cars());
+        }
+
+        [HttpGet("byColor")]
+        public ActionResult ByColor([FromQuery] string color)
+        {
+            return Ok(_carDatabase.ByColor(color));
+        }
+        [HttpPut]
+        public ActionResult Update([FromQuery] Guid id, [FromBody] Car car)
+        {
+            _carDatabase.Update(id, car);
+            return Ok();
+        }
+        [HttpDelete]
+        public ActionResult Delete(Guid id)
+        {
+            _carDatabase.Delete(id);
+            return Ok();
+        }
+    }
+}
